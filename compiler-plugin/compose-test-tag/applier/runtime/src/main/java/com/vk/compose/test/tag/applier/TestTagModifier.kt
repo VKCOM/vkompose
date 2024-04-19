58649bbb06126796e17207b0d8e8b37ce073e317
0xera <56160164+0xera@users.noreply.github.com>
2024-04-19 19:13:55 +0300

    migrate test tag applier to ModifierNodeElement

--- compiler-plugin/compose-test-tag/applier/runtime/src/main/java/com/vk/compose/test/tag/applier/TestTagModifier.kt
+++ compiler-plugin/compose-test-tag/applier/runtime/src/main/java/com/vk/compose/test/tag/applier/TestTagModifier.kt
@@ -2,18 +2,47 @@ package com.vk.compose.test.tag.applier
 
 import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.platform.debugInspectorInfo
-import androidx.compose.ui.platform.inspectable
-import androidx.compose.ui.semantics.semantics
+import androidx.compose.ui.node.ModifierNodeElement
+import androidx.compose.ui.node.SemanticsModifierNode
+import androidx.compose.ui.platform.InspectorInfo
+import androidx.compose.ui.semantics.SemanticsPropertyReceiver
 import androidx.compose.ui.semantics.testTag
 import androidx.compose.ui.semantics.testTagsAsResourceId
 
+fun Modifier.applyTestTag(tag: String): Modifier = then(TestTagElement(tag))
+
+private data class TestTagElement(
+    val tag: String,
+) : ModifierNodeElement<TestTagNode>() {
+    override fun create() = TestTagNode(tag)
+
+    override fun update(node: TestTagNode) {
+        node.tag = tag
+    }
+
+    override fun InspectorInfo.inspectableProperties() {
+        name = "composableTestTag"
+        properties["tag"] = tag
+    }
+
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (other !is TestTagElement) return false
+
+        return tag == other.tag
+    }
+
+    override fun hashCode(): Int {
+        return tag.hashCode()
+    }
+}
+
 @OptIn(ExperimentalComposeUiApi::class)
-fun Modifier.applyTestTag(tag: String): Modifier = inspectable(debugInspectorInfo {
-    name = "composableTestTag"
-    properties["tag"] = tag
-}) {
-    Modifier.semantics {
+private class TestTagNode(
+    var tag: String,
+) : Modifier.Node(), SemanticsModifierNode {
+
+    override fun SemanticsPropertyReceiver.applySemantics() {
         testTag = tag
         testTagsAsResourceId = true
     }
