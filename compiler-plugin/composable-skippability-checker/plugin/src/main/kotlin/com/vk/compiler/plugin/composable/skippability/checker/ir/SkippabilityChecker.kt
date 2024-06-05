d32f8a6ecd0d3c21fff7f41fee8fd44049cbcdb5
0xera <56160164+0xera@users.noreply.github.com>
2024-06-05 21:36:09 +0300

    migrate to k2

--- compiler-plugin/composable-skippability-checker/plugin/src/main/kotlin/com/vk/compiler/plugin/composable/skippability/checker/ir/SkippabilityChecker.kt
+++ compiler-plugin/composable-skippability-checker/plugin/src/main/kotlin/com/vk/compiler/plugin/composable/skippability/checker/ir/SkippabilityChecker.kt
@@ -9,6 +9,7 @@ import org.jetbrains.kotlin.backend.common.extensions.IrGenerationExtension
 import org.jetbrains.kotlin.backend.common.extensions.IrPluginContext
 import org.jetbrains.kotlin.cli.common.messages.CompilerMessageSeverity
 import org.jetbrains.kotlin.cli.common.messages.MessageCollector
+import org.jetbrains.kotlin.ir.ObsoleteDescriptorBasedAPI
 import org.jetbrains.kotlin.ir.declarations.IrModuleFragment
 import org.jetbrains.kotlin.ir.visitors.acceptChildrenVoid
 
@@ -25,7 +26,7 @@ class SkippabilityChecker(
         val fixedSuppressedFunctions = mutableSetOf<ReportFunction>()
 
         val stabilityInferencer = StabilityInferencer(
-            pluginContext.moduleDescriptor,
+            @OptIn(ObsoleteDescriptorBasedAPI::class) pluginContext.moduleDescriptor,
             stableTypeMatchers,
         )
 
